68c5adeb1191e76551530d57b2c82791a614a30e
Roman Farooq <TheRomanFarooq@gmail.com>
2022-10-14 03:48:50 +0500

    chnaged text-aligment

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -66,7 +66,7 @@ export default function Tshirts() {
           {tshirts &&
             tshirts.map((tshirt) => (
               <Link href="/product/t-shirt" key={tshirt.id}>
-                <div className="w-[70%] sm:w-[40%] md:w-[30%] lg:w-[20%] p-2 m-2 shadow-lg cursor-pointer">
+                <div className="w-[70%] sm:w-[40%] md:w-[30%] lg:w-[20%] p-3 m-2 shadow-lg cursor-pointer">
                   <a className="block relative rounded overflow-hidden">
                     <img
                       alt="ecommerce"
@@ -74,15 +74,15 @@ export default function Tshirts() {
                       src={tshirt.image}
                     />
                   </a>
-                  <div className="mt-4 text-center">
+                  <div className="mt-4 text-center sm:text-left">
                     <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
                       {tshirt.category.toUpperCase()}
                     </h3>
                     <h2 className="text-gray-900 title-font text-lg font-medium">
-                      {tshirt.name.charAt(0).toUpperCase() +
-                        tshirt.name.slice(1)}
+                      {tshirt.name.charAt(0).toUpperCase() + tshirt.name.slice(1)}
                     </h2>
-                    <p className="mt-1">${tshirt.price}</p>
+                    <p className="mt-1 text-sm">${tshirt.price}</p>
+                    <p className="mt-1 text-sm">S, M, L, XL, XXL</p>
                   </div>
                 </div>
               </Link>

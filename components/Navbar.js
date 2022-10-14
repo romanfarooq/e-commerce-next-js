22fbcdab16fd8f3584917c676c4ce1d4f029ec16
Roman Farooq <TheRomanFarooq@gmail.com>
2022-10-14 19:09:59 +0500

    made basic cart sidebar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,24 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { useRef } from 'react';
+import {
+  AiOutlineShoppingCart,
+  AiFillCloseCircle,
+  AiFillPlusCircle,
+  AiFillMinusCircle,
+} from 'react-icons/ai';
 
 export default function Navbar() {
+  const cart = useRef(null);
+
+  const toggleCart = () => {
+    if (cart.current.classList.contains('translate-x-full')) {
+      cart.current.classList.remove('translate-x-full');
+    } else {
+      cart.current.classList.add('translate-x-full');
+    }
+  };
+
   return (
     <nav className="flex p-2 flex-col md:flex-row md:justify-start justify-between items-center shadow-md">
       <div className="mx-5">
@@ -29,8 +45,38 @@ export default function Navbar() {
           </li>
         </ul>
       </div>
-      <div className="absolute right-0 top-5 mx-5">
+
+      <div
+        className="fixed right-0 top-3 mx-5 cursor-pointer rounded-full bg-white p-2 shadow-inner"
+        onClick={toggleCart}
+      >
         <AiOutlineShoppingCart className="text-2xl" />
+      </div>
+      <div
+        className="fixed top-0 right-0 h-full w-72 p-10 bg-indigo-100 transform transition-transform translate-x-full"
+        ref={cart}
+      >
+        <h2 className="font-bold text-xl text-center">Shopping Cart</h2>
+        <span
+          className="absolute top-5 right-2 cursor-pointer text-2xl text-indigo-500"
+          onClick={toggleCart}
+        >
+          <AiFillCloseCircle />
+        </span>
+        <ol className="list-decimal font-semibold">
+          <li>
+            <div className="flex my-5">
+              <div className="w-2/3 font-semibold">
+                T-Shirst -- Your Own Shop
+              </div>
+              <div className="w-1/3 font-semibold flex justify-center items-center">
+                <AiFillMinusCircle className="text-indigo-500 cursor-pointer" />
+                <span className="mx-2">1</span>
+                <AiFillPlusCircle className="text-indigo-500 cursor-pointer" />
+              </div>
+            </div>
+          </li>
+        </ol>
       </div>
     </nav>
   );

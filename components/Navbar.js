d20500e2cfaea3e33a465651da30071330d24ce3
Roman Farooq <TheRomanFarooq@gmail.com>
2022-10-15 19:36:53 +0500

    created cart functions

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,23 +10,23 @@ import {
 import { BsFillBagCheckFill } from 'react-icons/bs';
 import { MdDeleteForever } from 'react-icons/md';
 
-export default function Navbar() {
-  const cart = useRef(null);
+function Navbar({ cart, subTotal, clearCart, increaseQuantity, decreaseQuantity }) {
+  const cartRef = useRef(null);
 
   const toggleCart = () => {
-    if (cart.current.classList.contains('translate-x-full')) {
-      cart.current.classList.remove('translate-x-full');
+    if (cartRef.current.classList.contains('translate-x-full')) {
+      cartRef.current.classList.remove('translate-x-full');
     } else {
-      cart.current.classList.add('translate-x-full');
+      cartRef.current.classList.add('translate-x-full');
     }
   };
 
   return (
-    <nav className="flex p-2 flex-col md:flex-row md:justify-start justify-between items-center shadow-md">
+    <nav className="flex p-2 flex-col md:flex-row md:justify-start justify-between products-center shadow-md">
       
       <div className="mx-5">
         <Link href="/">
-          <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+          <a className="flex title-font font-medium products-center md:justify-start justify-center text-gray-900">
             <Image src="/Ecommerce.svg" alt="logo" width={40} height={40} />
             <span className="ml-3 text-xl text-primary">E-Commerce</span>
           </a>
@@ -58,8 +58,8 @@ export default function Navbar() {
       </div>
 
       <div
-        className="fixed z-10 overflow-x-hidden overflow-y-auto top-0 right-0 h-full w-80 p-10 bg-indigo-100 transform transition-transform translate-x-full"
-        ref={cart}
+        className="fixed z-10 overflow-x-hidden overflow-y-auto top-0 right-0 h-full w-80 p-9 bg-indigo-100 transform transition-transform translate-x-full"
+        ref={cartRef}
       >
         <h2 className="font-bold text-xl text-center">Shopping Cart</h2>
         <span
@@ -69,32 +69,49 @@ export default function Navbar() {
           <AiFillCloseCircle />
         </span>
         <ol className="list-decimal font-semibold">
-          <li>
-            <div className="flex my-5">
-              <div className="w-2/3 font-semibold">
-                T-Shirst -- Your Own Shop
-              </div>
-              <div className="w-1/3 font-semibold flex justify-center items-center">
-                <AiFillMinusCircle className="text-indigo-500 cursor-pointer" />
-                <span className="mx-2">1</span>
-                <AiFillPlusCircle className="text-indigo-500 cursor-pointer" />
-              </div>
-            </div>
-          </li>
+          {cart &&
+            cart.map((product) => (
+              <li key={product.id}>
+                <div className="flex my-5">
+                  <div className="w-2/3 font-semibold"><p>{product.name}</p></div>
+                  <div className="w-1/3 font-semibold flex justify-center products-center">
+                    <AiFillMinusCircle
+                      className="text-indigo-500 m-auto cursor-pointer"
+                      onClick={() => decreaseQuantity(product)}
+                    />
+                    <span className="mx-2">{product.quantity}</span>
+                    <AiFillPlusCircle
+                      className="text-indigo-500 m-auto cursor-pointer"
+                      onClick={() => increaseQuantity(product)}
+                    />
+                  </div>
+                </div>
+              </li>
+            ))}
         </ol>
 
-        <div className="flex justify-center mt-8">
+        <div className="flex justify-center my-3">
+          <span className="font-semibold">Subtotal:</span>
+          <span className="font-semibold mx-4">Rs {subTotal}</span>
+        </div>
+
+        <div className="flex justify-center mt-4">
           <button className="flex my-auto mr-5 text-white bg-indigo-500 border-0 p-3 focus:outline-none hover:bg-indigo-600 rounded text-sm">
             <BsFillBagCheckFill className="m-1" />
             Checkout
-          </button>   
-          <button className="flex my-auto text-white bg-indigo-500 border-0 p-3 focus:outline-none hover:bg-indigo-600 rounded text-sm">
+          </button>
+          <button
+            className="flex my-auto text-white bg-indigo-500 border-0 p-3 focus:outline-none hover:bg-indigo-600 rounded text-sm"
+            onClick={clearCart}
+          >
             <MdDeleteForever className="mt-[2px] text-lg" />
             Clear Cart
           </button>
         </div>
-
       </div>
+
     </nav>
   );
 }
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,77 @@
 import '../styles/globals.css';
+import { useState } from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
+  const [cart, setCart] = useState([
+    {
+      id: 1,
+      name: 'T-Shirt',
+      quantity: 1,
+      price: 1000,
+    },
+  ]);
+  const [subTotal, setSubTotal] = useState(
+    cart.reduce((acc, item) => acc + item.price * item.quantity, 0)
+  );
+
+  const addToCart = (product) => {
+    if (cart.length === 0) {
+      setCart([product]);
+      setSubTotal(product.price);
+    } else {
+      const newCart = [...cart];
+      const productIndex = newCart.findIndex((item) => item.id === product.id);
+      if (productIndex === -1) {
+        newCart.push(product);
+        setCart(newCart);
+        setSubTotal(subTotal + product.price);
+      } else {
+        newCart[productIndex].quantity += 1;
+        setCart(newCart);
+        setSubTotal(subTotal + product.price);
+      }
+    }
+  };
+
+  const increaseQuantity = (product) => {
+    const newCart = [...cart];
+    const productIndex = newCart.findIndex((item) => item.id === product.id);
+    newCart[productIndex].quantity += 1;
+    setCart(newCart);
+    setSubTotal(subTotal + product.price);
+  };
+
+  const decreaseQuantity = (product) => {
+    const newCart = [...cart];
+    const productIndex = newCart.findIndex((item) => item.id === product.id);
+    if (newCart[productIndex].quantity === 1) {
+      newCart.splice(productIndex, 1);
+      setCart(newCart);
+      setSubTotal(subTotal - product.price);
+    } else {
+      newCart[productIndex].quantity -= 1;
+      setCart(newCart);
+      setSubTotal(subTotal - product.price);
+    }
+  };
+
+  const clearCart = () => {
+    setCart([]);
+    setSubTotal(0);
+  };
+
   return (
     <>
-      <Navbar />
-      <Component {...pageProps} />
+      <Navbar
+        cart={cart}
+        subTotal={subTotal}
+        clearCart={clearCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+      />
+      <Component {...pageProps} addToCart={addToCart} />
       <Footer />
     </>
   );
